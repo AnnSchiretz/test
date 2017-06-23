57e43ca8ff43bf97a92931162f2adf05895ce051
AnnSchiretz <sisyrinchium02@gmail.com>
2017-06-23 12:22:07 +0300

    First action

--- gornichnaya.js
+++ gornichnaya.js
@@ -19,12 +19,19 @@ const base = require('./constants/baseFunctions')(CLIENT, WD);
 console.log(base);
 
 CLIENT.get(ASSETS.url).then(function(){
-    base.wait(1000, {name:'email'});
+    base.wait(10000, {name:'email'});
     base.getElement({name:'email'}).sendKeys(USER.email);
     base.getElement({name:'password'}).sendKeys(USER.pass);
     base.click(By.xpath('//*[@id="sign-in-form"]/div[3]/input'));
-    base.wait(1000, {xpath:'//*[@id="root"]/div/div/div/div/div[2]/button'});
+    base.wait(10000, {xpath:'//*[@id="root"]/div/div/div/div/div[2]/button'});
     base.click(By.xpath('//*[@id="root"]/div/div/div/div/div[2]/button'));
+    base.wait(10000,{xpath:'//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
+    base.click({xpath:'//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
+    base.click({xpath:'//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
+    // CLIENT.wait(WD.until.elementLocated({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[1]/input'}));
+    // base.click({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[1]/input'});
+    CLIENT.wait(WD.until.elementLocated({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[2]/input'}));
+    base.click({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[2]/input'});
 });
 
-CLIENT.quit();
+//CLIENT.quit();
