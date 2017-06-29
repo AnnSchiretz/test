4a2b76bb93fa49ea1bd97da31a536d184bb09b25
AnnSchiretz <sisyrinchium02@gmail.com>
2017-06-29 21:44:57 +0300

    pravki

--- gornichnaya_1.js
+++ gornichnaya_1.js
@@ -21,14 +21,16 @@ CLIENT.get(ASSETS.url).then(function(){
     base.getElement({name:'email'}).sendKeys(USER.email);
     base.getElement({name:'password'}).sendKeys(USER.pass);
     base.click(By.xpath('//*[@id="sign-in-form"]/div[3]/input'));
-    base.wait(2000, {xpath:'//*[@id="root"]/div/div/div/div/div[2]/button'});
-    base.click(By.xpath('//*[@id="root"]/div/div/div/div/div[2]/button'));
-    base.wait(2000,{xpath:'//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
-    base.click({xpath:'//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
-    base.click({xpath:'//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
+    base.waitTime(By.xpath('//*[@id="root"]/div/div/div/div/div[2]/button'));
+    base.click({xpath:'//*[@id="root"]/div/div/div/div/div[2]/button'});
+    base.waitTime(By.xpath('//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'));
+    base.click({xpath: '//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
+    base.click({xpath: '//*[@id="root"]/div/div/div/div[1]/div[2]/div[3]/a'});
+    // номер с неисправностями
     // CLIENT.wait(WD.until.elementLocated({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[1]/input'}));
     // base.click({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[1]/input'});
-    CLIENT.wait(WD.until.elementLocated({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[2]/input'}));
-    base.click({xpath:'//*[@id="root"]/div/div/div/div[3]/div[10]/center[2]/input'});
+    base.waitTime({xpath: '//*[@id="root"]/div/div/div/div[3]/div[10]/center[2]/input'});
+    base.click({xpath: '//*[@id="root"]/div/div/div/div[3]/div[10]/center[2]/input'});
+
 });
 //CLIENT.quit();
